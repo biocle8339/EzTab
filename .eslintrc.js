08823f79f17937a3750c78be48a85465be00a9bb
wonho <biocle8339@gmail.com>
2021-05-09 20:12:34 +0900

    SET: default setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: ["prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    "no-trailing-spaces": 0,
+    "keyword-spacing": 0,
+    "no-unused-vars": 1,
+    "no-multiple-empty-lines": 0,
+    "space-before-function-paren": 0,
+    "eol-last": 0,
+  },
+};
