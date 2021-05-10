0da11db35251b40fff816d5cd889ef828b66d60c
wonho lee <biocle8339@gmail.com>
2021-05-10 22:53:04 +0900

    WIP: popup html

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    webExtensions: true,
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
