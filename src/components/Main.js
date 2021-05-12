30a8acf81d70e100db64bbbfcee98e82838e18f0
Wonho Lee <biocle8339@gmail.com>
2021-05-12 20:23:46 +0900

    WIP changing screen with marker movements

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,59 @@ import Contents from "./Contents.js";
 import Navgation from "./Navigation.js";
 
 class Main extends Component {
+  setup() {
+    this.state = {
+      tabName: "currenttabs",
+      setTabName: this.addEvent("click", ".nav-link", function (event) {
+        const target =
+          event.target.tagName.toLowerCase() === "span" ? event.target : "x";
+
+        console.log(target);
+        if (event.target.hasChildNodes) {
+          this.setState({
+            ...this.state,
+            tabName: event.target
+              .querySelector("span")
+              .textContent.toLowerCase()
+              .replaceAll(/\s/g, ""),
+          });
+        }
+      }),
+    };
+  }
+
   template() {
-    return `<main></main>`;
+    return `
+    <main>
+      <div class="navigation-container"></div>
+    </main>
+    `;
+  }
+
+  setEvent() {
+    // this.addEvent("click", ".nav-link", function (event) {
+    //   if (event.target.hasChildNodes) {
+    //     console.log(
+    //       event.target.querySelector("span").textContent.toLowerCase().trim()
+    //     );
+    //     this.setState({
+    //       tabName: event.target
+    //         .querySelector("span")
+    //         .textContent.toLowerCase()
+    //         .trim(),
+    //     });
+    //   }
+    // });
   }
 
   mounted() {
     const $main = document.querySelector("main");
+    const $navigationContainer = document.querySelector(
+      ".navigation-container"
+    );
 
-    new Navgation($main);
-    new Contents($main);
+    new Navgation($navigationContainer, { setTabName: this.setState });
+    new Contents($main, { tabName: this.tabName });
   }
 }
 
